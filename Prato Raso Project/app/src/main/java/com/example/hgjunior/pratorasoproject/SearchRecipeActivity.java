f004c3066827ed9d45507a4eafa1e4dd593ab299
Mateus <mateus.pardinho@gmail.com>
2018-05-19 22:29:39 -0300

    Inclusão de classe e xml consulta de receitas
    Inclusão de classe e xml da tela de consulta de receitas

---
+++ Prato Raso Project/app/src/main/java/com/example/hgjunior/pratorasoproject/SearchRecipeActivity.java
@@ -0,0 +1,16 @@
+package com.example.hgjunior.pratorasoproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ *@author Mateus Dias Pardinho
+ */
+public class SearchRecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_recipe);
+    }
+}
